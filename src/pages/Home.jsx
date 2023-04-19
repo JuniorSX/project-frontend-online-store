6c1b14ffe439cdb5de8345222be78c8ec227a55f
Junior Souza <souza.jr91@gmail.com>
2023-04-19 20:28:07 -0300

    requisito 6

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import Categories from '../components/Categories/Categories';
+import ProductList from '../components/ProductList/ProductList';
+import { getCategoryId } from '../services/api';
 
 class ListagemDeProdutos extends Component {
   state = {
     redirect: false,
+    productsCategory: [],
   };
 
   handleButton = () => {
@@ -13,14 +16,19 @@ class ListagemDeProdutos extends Component {
     });
   };
 
+  getSelectedCategory = async ({ target: { id } }) => {
+    console.log('clicou', id);
+    const category = await getCategoryId(id);
+    this.setState({
+      productsCategory: category.results,
+    });
+  };
+
   render() {
-    const { redirect } = this.state;
+    const { redirect, productsCategory } = this.state;
     return (
       <div>
         {redirect && <Redirect to="/shopping-cart" />}
-        <section>
-          <Categories />
-        </section>
         <p
           data-testid="home-initial-message"
         >
@@ -29,6 +37,14 @@ class ListagemDeProdutos extends Component {
         <button onClick={ this.handleButton } data-testid="shopping-cart-button">
           Carrinho de compras
         </button>
+        <div className="main-section">
+          <section>
+            <Categories getSelectedCategory={ this.getSelectedCategory } />
+          </section>
+          <section>
+            <ProductList productsCategory={ productsCategory } />
+          </section>
+        </div>
       </div>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,12 @@ export async function getCategories() {
   return data;
 }
 
+export async function getCategoryId(categoryId) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+  const data = await response.json();
+  return data;
+}
+
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
   const data = await response.json();
