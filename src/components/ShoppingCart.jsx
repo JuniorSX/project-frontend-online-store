501518165cfdaf1ba63b118ad4895076218cb747
Carlos Leal <carlos.frleal@gmail.com>
2023-04-19 16:59:41 -0300

    requisito 3 em andamento

---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class ShoppingCart extends Component {
+  state = {
+    cartProducts: [],
+  };
+
+  render() {
+    const { cartProducts } = this.state;
+    return (
+      <div>
+        {!cartProducts
+          ? <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
+          : <div />}
+      </div>
+    );
+  }
+}
+
+export default ShoppingCart;
