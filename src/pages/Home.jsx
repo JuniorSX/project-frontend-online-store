c8e096a53f0ca413a8b954e263ee977b0f2cb2a3
Carlos Leal <carlos.frleal@gmail.com>
2023-04-19 20:37:38 -0300

    requisito 5 em andamento

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import Categories from '../components/Categories/Categories';
+import { getProductsFromQuery } from '../services/api';
 
 class ListagemDeProdutos extends Component {
   state = {
     redirect: false,
+    queryInput: '',
+    isQuery: false,
+    queryData: [],
   };
 
-  handleButton = () => {
+  handleButtonChart = () => {
     this.setState({
       redirect: true,
     });
   };
 
+  handleButtonQuery = async () => {
+    const { queryInput } = this.state;
+    const queryData = await getProductsFromQuery(queryInput);
+    // const tst = await getProductsFromCategoryAndQuery(0, queryInput);
+    const { results } = queryData;
+    this.setState({
+      queryData: results,
+      isQuery: results.length === 0,
+    });
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      queryInput: value,
+    });
+  };
+
   render() {
-    const { redirect } = this.state;
+    const { redirect, queryInput, isQuery, queryData } = this.state;
+    console.log(queryData);
     return (
       <div>
         {redirect && <Redirect to="/shopping-cart" />}
@@ -26,7 +49,26 @@ class ListagemDeProdutos extends Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
-        <button onClick={ this.handleButton } data-testid="shopping-cart-button">
+        <label htmlFor="query-input">
+          <input
+            type="text"
+            id="query-input"
+            data-testid="query-input"
+            name="queryInput"
+            value={ queryInput }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          data-testid="query-button"
+          onClick={ this.handleButtonQuery }
+        >
+          Pesquisar
+        </button>
+        {isQuery ? <h2>Nenhum produto foi encontrado</h2>
+          : <div>ja vou terminar</div>}
+
+        <button onClick={ this.handleButtonChart } data-testid="shopping-cart-button">
           Carrinho de compras
         </button>
       </div>
--- src/services/api.js
+++ src/services/api.js
@@ -10,6 +10,12 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return data;
 }
 
+export async function getProductsFromQuery(query) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const data = await response.json();
+  return data;
+}
+
 export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
