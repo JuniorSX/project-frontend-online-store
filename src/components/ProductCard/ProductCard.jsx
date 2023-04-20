2e2a9b3d10d63b273dbf1df1502299c3c2c69686
Junior Souza <souza.jr91@gmail.com>
2023-04-20 20:00:43 -0300

    requisito 11 html

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -46,6 +46,97 @@ export default class ProductCard extends Component {
             </div>
           </div>
         </section>
+        {/* https://metroui.org.ua/rating.html */}
+        <section className="rating-product">
+          <h2>Avaliação</h2>
+          <form>
+            <div className="form-group email-rating">
+              <input
+                type="email"
+                placeholder="Email"
+                data-testid="product-detail-email"
+              />
+              <input data-role="rating" data-value="3" />
+              <input type="radio" name="st" value="1" data-testid="1-rating" />
+              1
+              <input type="radio" name="st" value="2" data-testid="2-rating" />
+              2
+              <input type="radio" name="st" value="3" data-testid="3-rating" />
+              3
+              <input type="radio" name="st" value="4" data-testid="4-rating" />
+              4
+              <input type="radio" name="st" value="5" data-testid="5-rating" />
+              5
+
+            </div>
+            <div className="form-group">
+              <textarea
+                data-testid="product-detail-evaluation"
+                data-role="textarea"
+                placeholder="Mensagem (opcional)"
+                data-clear-button-icon="<span class='mif-cancel'></span>"
+              />
+            </div>
+            <div className="form-group">
+              <button
+                className="button success"
+                data-testid="submit-review-btn"
+              >
+                Avaliar
+              </button>
+            </div>
+          </form>
+        </section>
+        <section className="comments-product">
+          <div className="comment">
+            <p>junior@trybe.com</p>
+            <input
+              data-role="rating"
+              data-value="3.5"
+              data-static="true"
+            />
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat
+            </p>
+          </div>
+          <div className="comment">
+            <p>junior@trybe.com</p>
+            <input
+              data-role="rating"
+              data-value="3.5"
+              data-static="true"
+            />
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat
+            </p>
+          </div>
+          <div className="comment">
+            <p>junior@trybe.com</p>
+            <input
+              data-role="rating"
+              data-value="3.5"
+              data-static="true"
+            />
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat
+            </p>
+          </div>
+        </section>
       </>
     );
   }
