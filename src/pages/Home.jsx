5501b522c0fafcff43476e3bdeb5212343fa2151
Carlos Leal <carlos.frleal@gmail.com>
2023-04-19 21:02:07 -0300

    requisito 5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,6 @@ class ListagemDeProdutos extends Component {
     queryInput: '',
     isQuery: false,
     queryData: [],
-    productsCategory: [],
   };
 
   handleButtonChart = () => {
@@ -41,12 +40,12 @@ class ListagemDeProdutos extends Component {
     console.log('clicou', id);
     const category = await getCategoryId(id);
     this.setState({
-      productsCategory: category.results,
+      queryData: category.results,
     });
   };
 
   render() {
-    const { redirect, queryInput, isQuery, queryData, productsCategory } = this.state;
+    const { redirect, queryInput, isQuery, queryData } = this.state;
     console.log(queryData);
     return (
       <div>
@@ -73,19 +72,24 @@ class ListagemDeProdutos extends Component {
           Pesquisar
         </button>
         {isQuery ? <h2>Nenhum produto foi encontrado</h2>
-          : <div>ja vou terminar</div>}
-
-        <button onClick={ this.handleButtonChart } data-testid="shopping-cart-button">
-          Carrinho de compras
-        </button>
-        <div className="main-section">
-          <section>
-            <Categories getSelectedCategory={ this.getSelectedCategory } />
-          </section>
-          <section>
-            <ProductList productsCategory={ productsCategory } />
-          </section>
-        </div>
+          : (
+            <>
+              <button
+                onClick={ this.handleButtonChart }
+                data-testid="shopping-cart-button"
+              >
+                Carrinho de compras
+              </button>
+              <div className="main-section">
+                <section>
+                  <Categories getSelectedCategory={ this.getSelectedCategory } />
+                </section>
+                <section>
+                  <ProductList queryData={ queryData } />
+                </section>
+              </div>
+            </>
+          )}
       </div>
     );
   }
